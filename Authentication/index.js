d165ee3996d0427ffc9314a3a2978607514f978b
Raj kashyap <rajkashyap.ki05@gmail.com>
2026-04-22 10:30:26 +0530

    updated

--- Authentication/index.js
+++ Authentication/index.js
@@ -2,7 +2,7 @@ import express, { Router } from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./src/condig/db.js";
 import router from "./src/router/user.route.js";
-import passport from "./src/condig/passport.js"
+// import passport from "./src/condig/passport.js"
 import session from "express-session";
 
 dotenv.config();
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 6000;
 app.use(express.json());
 
 app.use(session({ secret: "session_secret", resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 
 app.use("/auth", router);
